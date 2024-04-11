07262ae4f904a19ce73012ed2d499b77e00fa46c
Tran Thanh Dat <neban0444@gmail.com>
2024-04-11 18:30:29 +0700

    get request by user

--- controller/request_controller.js
+++ controller/request_controller.js
@@ -1,6 +1,8 @@
 const UserWallet = require('../model/UserWallet.js');
 const Request = require('../model/Request.js');
 const helper = require('../pkg/helper/helper.js');
+const User = require('../model/User.js');
+const { get } = require('mongoose');
 
 const response = async (req, res) => {
     const requestId = req.body.requestId;
@@ -28,6 +30,25 @@ const response = async (req, res) => {
     
 }
 
+const getByUser = async (req, res) => {
+    const userId = req.params.id;
+    const isValidId = await helper.isValidObjectID(userId);
+    if(!isValidId) return res.status(400).json({
+        message: "Invalid user id"
+    })
+    const existUser = await User.findById(userId).select('-password')
+    if(!existUser) return res.status(404).json({
+        message: "User is not found"
+    })
+    const requests = await Request.find({
+        receiverId: userId
+    })
+    return res.json({
+        data: requests
+    })
+}
+
 module.exports = {
-    response
+    response,
+    getByUser
 }
--- route/request_route.js
+++ route/request_route.js
@@ -2,6 +2,8 @@ const requestController = require('../controller/request_controller');
 const express = require('express');
 const route = express.Router();
 
+
 route.post('/response', requestController.response);
+route.get('/getbyuser/:id', requestController.getByUser);
 
 module.exports = route;
